d7cb29535ecd098ca657cdaaa342b82013e3fe6b
Andressa Reis Araujo <andressareis98@outlook.com>
2023-06-21 22:40:57 -0300

    adicionando as imagens dos cafés e criando um mock

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,17 @@
+import { coffees } from "./../../mocks/coffees";
+
 export function Home() {
-  return <div>Home</div>;
+  const initialSrc = "../../../public/assets/images/";
+  return (
+    <div>
+      {coffees.map((item) => {
+        return (
+          <div key={item.id}>
+            <h1>{item.name}</h1>
+            <img src={`${initialSrc}${item.src}`} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
