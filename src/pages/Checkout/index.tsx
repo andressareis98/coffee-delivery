7e37041bad6bbd1624113ba0feedf059ad67ed94
Andressa Reis Araujo <andressareis98@outlook.com>
2023-08-27 12:06:39 -0300

    feat: adicionando validação de quando o botão de confirmar pode ser clicado

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -11,16 +11,35 @@ import { PaymentForm } from "./Components/PaymentForm";
 import { CoffeesList } from "./Components/CoffeesList";
 import { Values } from "./Components/Values";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { CoffeesContext } from "../../contexts/CoffeesContext";
 import { AddressContext } from "../../contexts/AddressContext";
+import { PaymentTypeContext } from "../../contexts/PaymentTypeContext";
 
 export function Checkout() {
-  const { removerTudo } = useContext(CoffeesContext);
-  const { removeAddress } = useContext(AddressContext);
+  const { coffees, removerTudo } = useContext(CoffeesContext);
+  const { address, removeAddress } = useContext(AddressContext);
+  const { paymentType } = useContext(PaymentTypeContext);
+
+  const navigate = useNavigate();
+
+  const addressIsComplete = Boolean(
+    address.cep &&
+      address.city &&
+      address.neighborhood &&
+      address.number &&
+      address.street
+  );
+
+  const paymentTypeIsComplete = Boolean(paymentType);
+
+  const hasCoffees = Boolean(coffees.length);
+
+  const isComplete = addressIsComplete && paymentTypeIsComplete && hasCoffees;
 
   const handleConfirmOrder = () => {
+    navigate("/success");
     removerTudo();
     removeAddress();
   };
@@ -38,9 +57,12 @@ export function Checkout() {
           <CoffeesList />
           <Values />
 
-          <Link to="/success" onClick={handleConfirmOrder}>
-            <ConfirmOrderButton>Confirmar pedido</ConfirmOrderButton>
-          </Link>
+          <ConfirmOrderButton
+            disabled={!isComplete}
+            onClick={handleConfirmOrder}
+          >
+            Confirmar pedido
+          </ConfirmOrderButton>
         </PurchaseDataContainer>
       </ColumnContainer>
     </Container>
